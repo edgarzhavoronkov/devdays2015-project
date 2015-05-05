24c38996d46148df27591069e6ca43b79f15bedc
Ivan <ivan.afonichkin@gmail.com>
2015-05-05 16:16:39 +0300

    Hands and Head

--- test/test.ino
+++ test/test.ino
@@ -1,44 +1,125 @@
-// Sweep
-// by BARRAGAN <http://barraganstudio.com> 
-// This example code is in the public domain.
-
-
 #include <Servo.h> 
  
-Servo myservo1, myservo2;                  
- 
-int pos = 0;    
+const int COMMAND_BLINK_ON = 1;
+const int COMMAND_EYES_ON = 2;
+const int COMMAND_EYES_OFF = 3;
+const int COMMAND_MOVE_LH = 4;
+const int COMMAND_MOVE_RH = 5;
+const int COMMAND_MOVE_HEAD = 6;
+
+Servo HeadServo;
+Servo LeftHandServo;
+Servo RightHandServo;
+
+int HeadServoPin = 9;
+int LeftHandServoPin = 10;
+int RightHandServoPin = 11;
+
+int leftHandCurrent;
+int leftHandTarget;
+
+int rightHandCurrent;
+int rightHandTarget;
+
+int headCurrent;
+int headTarget;
+
+void MoveLeftHand()
+{
+  if (leftHandCurrent == leftHandTarget)
+    break;
+    
+  int dx = 1;
+  
+  if (leftHandCurrent > leftHandTarget)
+    dx *= -1;
+    
+  leftHandCurrent += dx;
+    
+  LeftHandServo.write(leftHandCurrent);
+}
+
+void MoveRightHand()
+{
+  if (rightHandCurrent == rightHandTarget)
+    break;
+    
+  int dx = 1;
+  
+  if (rightHandCurrent > rightHandTarget)
+    dx *= -1;
+    
+  rightHandCurrent += dx;
+    
+  RightHandServo.write(rightHandCurrent);
+}
+
+void MoveHead()
+{
+  if (headCurrent == headTarget)
+    break;
+    
+  int dx = 1;
+  
+  if (headCurrent > headTarget)
+    dx *= -1;
+    
+  headCurrent += dx;
+    
+  HeadServo.write(headCurrent);
+}
+
+void Parse()
+{
+  int command = 0;
+  int angle = 0;
+  
+  if (Serial.avaliable() > 0)
+  {
+    command = Serial.read()
+  } else
+  {
+    break;
+  }
+  
+  //need to read one more parameter
+  if (command == COMMAND_MOVE_LH || command == COMMAND_MOVE_RH || command == COMMAND_MOVE_HEAD)
+  {
+    angle = Serial.read();
+  }
+  
+  if (command == COMMAND_MOVE_LH)
+  {
+    leftHandTarget = angle;
+  } else if (command == COMMAND_MOVE_RH)
+  {
+    rightHandTarget = angle;
+  } else if (command == COMMAND_MOVE_HEAD)
+  {
+    headTarget = angle;
+  }
+      
+}
  
 void setup() 
 { 
-  myservo1.attach(9);
-  myservo2.attach(10);
+  HeadServo.attach(HeadServoPin);
+  LeftHandServo.attach(LeftHandServoPin);
+  RightHandServo.attach(RightHandServoPin);
+  
+  leftHandCurrent = leftHandTarget = 0;
+  rightHandCurrent = rightHandTarget = 0;
+  headCurrent = headHandTarget = 0;
 } 
  
  
 void loop() 
 { 
-  for(pos = 0; pos < 180; pos += 1)  
-  {                                  
-    myservo1.write(pos);              
-    delay(15);                       
-  } 
-  for(pos = 180; pos>=1; pos-=1)     
-  {                                
-    myservo1.write(pos);              
-    delay(15);                      
-  } 
-  
-  delay(100);
-  
-  for(pos = 0; pos < 180; pos += 1)  
-  {                                  
-    myservo2.write(pos);              
-    delay(15);                       
-  } 
-  for(pos = 180; pos>=1; pos-=1)     
-  {                                
-    myservo2.write(pos);              
-    delay(15);                      
-  }   
+  Serial.begin(9600); 
+
+  Parse();
+
+  MoveHead();
+  MoveLeftHand();
+  MoveRightHand();  
 } 
